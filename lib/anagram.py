4577ae213fcf7e52c59553aa85c0a141f4c7c723
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-05-14 15:10:29 +0300

    completed anagrsm detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word =word.lower()
+        self.word_sorted = sorted(self.word)
+    def match(self,word_list):
+        anagrams = []
+        for word in word_list:
+            if self.word_sorted == sorted(word.lower()) and self.word != word.lower():
+                anagrams.append(word)
+        return anagrams
+# Example usage:
+anagram = Anagram("listen")
+print(anagram.match(["enlist", "google", "inlets", "banana"]))
+# Output: ['enlist', 'inlets']
